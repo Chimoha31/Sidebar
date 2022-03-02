aee1df39acd098c83d7136c64fa1c071eba3dfac
Chimoha31 <mokochii1108@gmail.com>
2022-03-02 10:28:10 -0800

    Sidebar component

---
+++ src/components/Sidebar.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Sidebar() {
+  return (
+    <div className="Sidebar">
+      <ul className="SidebarList">
+        {/* liにはicon, listtitle, linkの属性が含まれる。1つ1つのliに1つ1つ書いてくのは面倒なので、その属性をcomponentとして書き出していく→SidebarData.jsへ */}
+      </ul>
+    </div>
+  )
+}
+
+export default Sidebar
