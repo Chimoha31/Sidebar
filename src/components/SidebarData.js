2f57bed6b84d8ef298d083dee13725049e8b6712
Chimoha31 <mokochii1108@gmail.com>
2022-03-02 10:52:28 -0800

    installed material ui

---
+++ src/components/SidebarData.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import HomeIcon from '@mui/icons-material/Home';
+
+// dataだけを格納していくので、このcomponentはdataのみ
+export const SidebarData = [
+  {
+    title: "Home",
+    icon: <HomeIcon />,
+    link: "/home"
+  }
+]
+
+
