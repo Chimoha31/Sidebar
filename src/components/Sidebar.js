f8c12c7536c6c81160b2dc38ff5b66461ecaa9cd
Chimoha31 <mokochii1108@gmail.com>
2022-03-02 11:04:28 -0800

    map for each list

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,10 +1,21 @@
 import React from 'react'
+import {SidebarData} from './SidebarData';
 
 function Sidebar() {
   return (
     <div className="Sidebar">
       <ul className="SidebarList">
         {/* liにはicon, listtitle, linkの属性が含まれる。1つ1つのliに1つ1つ書いてくのは面倒なので、その属性をcomponentとして書き出していく→SidebarData.jsへ */}
+        {SidebarData.map((value, key) => {
+          return(
+            <li key={key} className="row" onClick={() => {
+              window.location.pathname = value.link
+            }}>
+              <div id="icon">{value.icon}</div>
+              <div id="icon">{value.title}</div>
+            </li>
+          )
+        })}
       </ul>
     </div>
   )
--- src/components/SidebarData.js
+++ src/components/SidebarData.js
@@ -1,13 +1,47 @@
-import React from 'react'
-import HomeIcon from '@mui/icons-material/Home';
+import React from "react";
+import HomeIcon from "@mui/icons-material/Home";
+import AttachEmailIcon from "@mui/icons-material/AttachEmail";
+import AssessmentIcon from "@mui/icons-material/Assessment";
+import PersonAddIcon from "@mui/icons-material/PersonAdd";
+import AddCardIcon from "@mui/icons-material/AddCard";
+import BackupIcon from "@mui/icons-material/Backup";
+import SettingsIcon from "@mui/icons-material/Settings";
 
 // dataだけを格納していくので、このcomponentはdataのみ
 export const SidebarData = [
   {
     title: "Home",
     icon: <HomeIcon />,
-    link: "/home"
-  }
-]
-
-
+    link: "/home",
+  },
+  {
+    title: "Mail",
+    icon: <AttachEmailIcon />,
+    link: "/mail",
+  },
+  {
+    title: "Analitics",
+    icon: <AssessmentIcon />,
+    link: "/analitics",
+  },
+  {
+    title: "Add friends",
+    icon: <PersonAddIcon />,
+    link: "/friends",
+  },
+  {
+    title: "Setting for payment",
+    icon: <AddCardIcon />,
+    link: "/payment",
+  },
+  {
+    title: "Upload",
+    icon: <BackupIcon />,
+    link: "/upload",
+  },
+  {
+    title: "詳細設定",
+    icon: <SettingsIcon />,
+    link: "/rocket",
+  },
+];
